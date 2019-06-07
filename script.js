a031694dfb2262751b1d2882d703155fc64bec9e
Stephan <sgriesel@gmail.com>
2019-06-07 16:48:12 +0200

    finish

---
+++ script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullPage', {
+    autoScrolling:true,
+    navigation:true,
+    anchors: ['section1', 'section2', 'section3'],
+    navigationTooltips: ['HOME', 'ABOUT US', 'CONTACT'],
+    showActiveTooltip: true,
+    scrollingSpeed:2000,
+    controlArrows:false,
+    slidesNavigation:true
+}) 
